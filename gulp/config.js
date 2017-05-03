e25750e67b69aa025a832cd87a9d67de1faf0a0e
Петров Никита <petrov-ns@gamma-center.ru>
2017-05-03 17:05:51 +0300

    [change] - config.js

--- gulp/config.js
+++ gulp/config.js
@@ -4,9 +4,10 @@
 import path from 'path';
 
 /* Constants */
-const SOURCE = 'src';
-const DEST   = 'dest';
-const APP    = path.join(SOURCE, 'app');
+const source   = 'source';
+const app      = path.join(source, 'app');
+const devDest  = 'dev-dest';
+const prodDest = 'prod-dest';
 
 /* Build config */
 const config = {
@@ -15,19 +16,21 @@ const config = {
 
 /* App paths */
 const paths = {
-  js:        path.join(APP, '**/*!(.spec.js).js'),
-  main:      path.join(APP, 'main.js'),
-  index:     path.join(SOURCE, 'index.html'),
-  templates: path.join(APP, '**/*.html'),
-  stylus:    path.join(APP, '**/*.stylus'),
+  js:        path.join(app, '**/*!(.spec.js).js'),
+  index:     path.join(source, 'index.html'),
+  fonts:     path.join(source, 'fonts/**/*'),
+  test:      path.join(app, '**/*.spec.js'),
+  stylus:    path.join(app, '**/*.stylus'),
+  img:       path.join(source, 'img/**/*'),
+  templates: path.join(app, '**/*.html'),
+  main:      path.join(app, 'main.js'),
   pug: [
-    path.join(APP, '**/*.pug'),
-    path.join(SOURCE, 'index.pug')
+    path.join(source, 'index.pug'),
+    path.join(app, '**/*.pug')
   ],
-  img:   path.join(SOURCE, 'img/**/*'),
-  fonts: path.join(SOURCE, 'fonts/**/*'),
-  dest:  DEST,
-  src:   SOURCE
+  prodDest,
+  devDest,
+  source
 };
 
 export {paths, config};
