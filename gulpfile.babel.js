f3069207f089a3000f34bf4afe9a69b2074ee692
Петров Никита <petrov-ns@gamma-center.ru>
2017-05-16 17:16:33 +0500

    gulpfile - добавил build:css в build

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,12 +4,14 @@
 import gulp from 'gulp';
 
 /* Tasks imports */
-import './gulp/tasks/templates'; // build-templates
-import './gulp/tasks/index';     // build-index
-import './gulp/tasks/vendor';     // build-vendors
-import './gulp/tasks/scripts';     // build-js
+import './gulp/tasks/templates'; // build:templates
+import './gulp/tasks/index';     // build:index
+import './gulp/tasks/vendor';    // build:vendors
+import './gulp/tasks/scripts';   // build:js
+import './gulp/tasks/styles';    // build:css
 
 gulp.task('build', gulp.series(
   gulp.parallel('build-vendor:js', 'build-vendor:css'),
-  gulp.parallel('build-index', 'build-templates')
+  gulp.parallel('build:js', 'build:css'),
+  gulp.parallel('build:index', 'build:templates')
 ));
