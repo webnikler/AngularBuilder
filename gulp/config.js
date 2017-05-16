2b0b8969b28b1dc3e6b6167039f50e45cf3349c4
Петров Никита <petrov-ns@gamma-center.ru>
2017-05-16 17:17:14 +0500

    config.js - изменил структуру путей

--- gulp/config.js
+++ gulp/config.js
@@ -11,38 +11,41 @@ const app    = path.join(source, 'app');
 const dev    = 'dev';
 const prod   = 'prod';
 
-/* Plugins */
+/* Other */
 const plugins = glp();
-
-/* Args */
 const args = yargs.argv;
 
 /* Build config */
 const config = {
-  env: args.env || 'prod',
+  isDevelop:   args.env === 'dev',
   useMaterial: true // Если ставится false ,требуется дополнительно включить флаг ignore для material-design в bower.json
 };
 
-/* Webpack paths */
-const webpackPaths = {
-  input:  `./${source}/app/main.js`,
-  output: `/dev/js`
-}
-
 /* App paths */
 const paths = {
-  js:        path.join(app, '**/*!(.spec.js).js'),
-  index:     path.join(source, 'index.pug'),
-  fonts:     path.join(source, 'fonts/**/*'),
-  test:      path.join(app, '**/*.spec.js'),
-  stylus:    path.join(app, '**/*.stylus'),
-  img:       path.join(source, 'img/**/*'),
-  templates: path.join(app, '**/*.pug'),
-  main:      path.join(app, 'main.js'),
-  webpack:   webpackPaths,
-  source,
-  prod,
-  dev
+  source: {
+    files: {
+      scipts:    path.join(app, '**/*!(.spec.js).js'),
+      indexHTML: path.join(source, 'index.pug'),
+      fonts:     path.join(source, 'fonts/**/*'),
+      test:      path.join(app, '**/*.spec.js'),
+      stylus:    path.join(app, 'main.styl'),
+      images:    path.join(source, 'img/**/*'),
+      templates: path.join(app, '**/*.pug'),
+      mainJS:    path.join(app, 'main.js')
+    },
+    folders: {
+      scripts:   app,
+      base:      source
+    }
+  },
+  dest: {
+    folders: {
+      base:      (config.isDevelop ? dev : prod),
+      scripts:   (config.isDevelop ? `${dev}/js` : `${prod}/js`),
+      styles:    (config.isDevelop ? `${dev}/css` : `${prod}/css`)
+    }
+  }
 };
 
 export {paths, config, plugins};
