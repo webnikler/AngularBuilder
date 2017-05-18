1f3a4742b9508abde1b750be416aaa52be0b1cd4
Петров Никита <petrov-ns@gamma-center.ru>
2017-05-18 17:32:29 +0300

    Отладка таска build-vendor:js

--- gulp/tasks/vendor.js
+++ gulp/tasks/vendor.js
@@ -5,13 +5,17 @@ import path       from 'path';
 import bowerFiles from 'main-bower-files';
 import {paths, plugins, config} from '../config';
 
-const vendors = bowerFiles();
+const vendors = bowerFiles({debugging: true});
 const isProduction = !config.isDevelop
 
 gulp.task('build-vendor:js', () => {
+
+  console.log('VENDORS_INIT', vendors);
+
   return gulp.src(vendors)
     .pipe( plugins.filter('**/*.js') )
     .pipe( plugins.order(vendors) )
+    .pipe( plugins.debug({title: 'order'}) )
     .pipe( plugins.concat('vendor.js') )
     .pipe( plugins.if( isProduction, plugins.uglify() ) )
     .pipe( plugins.if( isProduction, plugins.rename({extname: '.min.js'}) ) )
