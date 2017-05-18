f77c1bb0622bd8e2af8a8ebe2e99ffec0ec34fde
Nikler <webnikler@gmail.com>
2017-05-19 00:43:44 +0300

    build-vendor:js -> заменил orderArgs на простые аргументы

--- gulp/tasks/vendor.js
+++ gulp/tasks/vendor.js
@@ -6,20 +6,14 @@ import bowerFiles from 'main-bower-files';
 import {paths, plugins, config} from '../config';
 
 const vendors = bowerFiles();
+const relativePaths = vendors.map(vendor => vendor.match(/bower_components\S+/)[0]);
 const isProduction = !config.isDevelop
-
-const orderArgs = [
-  /* Перевод абсолютных путей в относительные (иначе gulp-order не съест их) */
-  vendors.map(vendor => vendor.match(/bower_components\S+/)[0]),   
-  { base: './' }
-];
-
 const rename = { suffix: '.min' };
 
 gulp.task('build-vendor:js', () => {
   return gulp.src(vendors)
     .pipe( plugins.filter('**/*.js') )
-    .pipe( plugins.order(...orderArgs) )
+    .pipe( plugins.order(relativePaths, { base: './' }) )
     .pipe( plugins.debug({ title: 'order' }) )
     .pipe( plugins.concat('vendor.js') )
     .pipe( plugins.if( isProduction, plugins.uglify() ) )
