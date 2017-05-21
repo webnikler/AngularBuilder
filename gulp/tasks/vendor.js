5562a14608c557e41202f46a21cfb31008eacbf6
Петров Никита <petrov-ns@gamma-center.ru>
2017-05-21 22:33:31 +0300

    Рефакторинг tasks/vendors

--- gulp/tasks/vendor.js
+++ gulp/tasks/vendor.js
@@ -2,29 +2,33 @@
 
 import gulp from 'gulp';
 import bowerFiles from 'main-bower-files';
+import combine from 'stream-combiner';
 import {paths, plugins, config} from '../config';
 
 const vendors = bowerFiles();
 const relativePaths = vendors.map(vendor => vendor.match(/bower_components\S+/)[0]);
-const isProduction = !config.isDevelop
-const rename = { suffix: '.min' };
+const isProduction = !config.isDevelop;
+const isExtJs = file => file.extname === '.js';
 
-gulp.task('build-vendor:js', () => {
+export default function $build_vendors() {
   return gulp.src(vendors)
-    .pipe( plugins.filter('**/*.js') )
-    .pipe( plugins.order(relativePaths, { base: './' }) )
-    .pipe( plugins.debug({ title: 'order' }) )
-    .pipe( plugins.concat('vendor.js') )
-    .pipe( plugins.if( isProduction, plugins.uglify() ) )
-    .pipe( plugins.if( isProduction, plugins.rename(rename) ) )
-    .pipe( gulp.dest(paths.dest.folders.scripts) );
-});
-
-gulp.task('build-vendor:css', () => {
-  return gulp.src(vendors)
-    .pipe( plugins.filter('**/*.css') )
-    .pipe( plugins.concat('vendor.css') )
-    .pipe( plugins.if( isProduction, plugins.csso() ) )
-    .pipe( plugins.if( isProduction, plugins.rename(rename) ) )
-    .pipe( gulp.dest(paths.dest.folders.styles) );
-});
+    .pipe(plugins.filter('**/*.{js,css}'))
+    .pipe(plugins.if(isExtJs, 
+      combine(
+        plugins.order(relativePaths, { base: './' }),
+        plugins.concat('vendor.js'),
+        plugins.if(isProduction, plugins.uglify())
+      ),
+      combine(
+         plugins.concat('vendor.css'),
+         plugins.if(isProduction, plugins.csso())
+      )
+    ))
+    .pipe(plugins.if(isProduction, plugins.rename({suffix: '.min'})))
+    .pipe(plugins.debug({title:'combine'}))
+    .pipe(gulp.dest(file => {
+      return isExtJs(file) ?
+        paths.dest.folders.scripts :
+        paths.dest.folders.styles;
+    }))
+}
