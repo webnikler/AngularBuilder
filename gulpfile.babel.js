01240522be4370625b5b0d273995700412c3d644
Петров Никита <petrov-ns@gamma-center.ru>
2017-05-21 22:38:21 +0300

    gulpfile -> рефакторинг

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,21 +1,32 @@
 'use strict';
 
-/* Common imports */
 import gulp from 'gulp';
+import del  from 'del';
+import path from 'path';
+import {paths, plugins, config} from './gulp/config';
 
-/* Tasks imports */
-import './gulp/tasks/index';   // build:index
-import './gulp/tasks/vendor';  // build:vendors
-import './gulp/tasks/scripts'; // build:js
-import './gulp/tasks/styles';  // build:css
-import './gulp/tasks/serve';   // serve
+import $build_indexFile from './gulp/tasks/index';
+import $build_vendors   from './gulp/tasks/vendor';
+import $build_scripts   from './gulp/tasks/scripts';
+import $build_styles    from './gulp/tasks/styles';
+import $build_other     from './gulp/tasks/other';
+import $run_server      from './gulp/tasks/serve';
 
-gulp.task('build', gulp.series(
-  gulp.parallel(
-    'build-vendor:js',
-    'build-vendor:css',
-    'build:js',
-    'build:css'
-  ),
-  'build:index'
-));
+const $run_clean = () => del(paths.dest.folders.base);
+const $run_watch = () => {
+  gulp.watch(paths.source.files.stylus, gulp.series($build_styles));
+  gulp.watch([paths.source.files.fonts, paths.source.files.images], gulp.series($build_other))
+    .on('unlink', filepath => {
+      delete plugins.cached.caches.$build_other[path.resolve(filepath)];
+    });
+};
+
+const buildSeries = [
+  $run_clean,
+  gulp.parallel($build_vendors, $build_scripts, $build_styles, $build_other),
+  $build_indexFile
+];
+
+if (config.isDevelop) buildSeries.push(gulp.parallel($run_watch, $run_server));
+
+gulp.task('$build', gulp.series(...buildSeries));
