777ae6523a760bfca1a569e6cbfbb7cd368adf8b
Петров Никита <petrov-ns@gamma-center.ru>
2017-05-21 22:36:38 +0300

    tasks/index - изменил функцию inject (теперь get)

--- gulp/tasks/index.js
+++ gulp/tasks/index.js
@@ -5,31 +5,29 @@ import path from 'path';
 import _    from 'lodash';
 import {paths, plugins, config} from '../config';
 
-/** @description - Функция для преобразования аргументов для плагина gulp-inject
- * 
- * @param {string} _path - Путь до файла без расширения
- * @param {string} _ext - Расширение файла
- * @param {object} _options - Дополнительные опции для плагина gulp-inject
- * @return {array}  - Возвращает массив из элементов [Путь к файлу с расширением, Опции]
- */
-const inject = (_path, _ext, _options = {}) => {
-  const ext  = config.isDevelop ? _ext : `min.${_ext}`;
-  const file = `${_path}.${ext}`;
-  const name = _path.match(/\w+$/)[0];
-  const base = paths.dest.folders.base;
-
-  const source = gulp.src(path.join(base, file), {read: false});
-  const options = _.merge({ignorePath: base, name}, _options);
-
-  return [source, options];
+export default function $build_indexFile() {
+  return gulp.src(paths.source.files.indexHTML)
+    .pipe( plugins.pug(config.isDevelop ? {pretty: true} : null) )
+    .pipe( plugins.inject(...get('js/vendor.js')) )
+    .pipe( plugins.inject(...get('css/vendor.css')) )
+    .pipe( plugins.inject(...get('js/build.js')) )
+    .pipe( plugins.inject(...get('css/build.css')) )
+    .pipe( gulp.dest(paths.dest.folders.base) );
 }
 
-gulp.task('build:index',  () => {
-  return gulp.src(paths.source.files.indexHTML)
-    .pipe( plugins.pug( config.isDevelop ? {pretty: true} : null ) )
-    .pipe( plugins.inject(...inject('js/vendor', 'js')) )
-    .pipe( plugins.inject(...inject('css/vendor', 'css')) )
-    .pipe( plugins.inject(...inject('js/build', 'js')) )
-    .pipe( plugins.inject(...inject('css/build', 'css')) )
-    .pipe( gulp.dest(paths.dest.folders.base) );
-});
+function get(_path, _options = {}) {
+  const fileName = _path.match(/\w+.\w+$/)[0];
+  const dirName = _path.slice(0, _path.lastIndexOf(fileName));
+  const extName = fileName.slice(fileName.indexOf('.'));
+  const stem = fileName.slice(0, fileName.indexOf('.'));
+  const base = paths.dest.folders.base;
+
+  const newFileName = config.isDevelop ?
+    `${dirName}${stem}*${extName}` :
+    `${dirName}${stem}*.min${extName}`;
+
+  const source = gulp.src(path.join(base, newFileName), {read: false});
+  const options = _.merge({ignorePath: base, name: stem}, _options);
+
+  return [source, options];
+};
